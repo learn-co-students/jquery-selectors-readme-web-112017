b99e4a37a74e14d8b1c901353ea615e04235a5b7
KellyStamps <kellydsample@gmail.com>
2018-01-15 17:29:08 -0500

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,41 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
+
+  // $('img')
+  // //returns array of 6 images
+  //
+  // $('.pics')
+  // //selects all things with class of pics
+  //
+  // $('#baby-ninja')
+  // //selects the thing with this id
+  //
+  // $('ul li')
+  // //selects all items inside of uls
+  //
+  // $('div img:first-child')
+  // //selects the first image in a img div
+  //
+  // $('img[alt='the beatles making faces']')
+  // //selects image with the alternate text matching
+  //
+  // $('div:last')
+  //last div on page
